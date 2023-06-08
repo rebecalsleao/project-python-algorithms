5552367d961c56db066e6dae0c943788c778484e
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-06-08 13:20:33 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,22 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    
+    score = 0
+
+    for period in permanence_period:
+        arrival_time = period[0]
+        departure_time = period[1]
+        arrival_time_is_invalid = type(arrival_time) != int 
+        departure_time_is_invalid = type(departure_time) != int
+        target_time_is_invalid = type(target_time) != int 
+
+
+        if (arrival_time_is_invalid or departure_time_is_invalid or target_time_is_invalid):
+            return None
+        
+        arrived_before_or_on_time = arrival_time <= target_time
+        left_after_or_on_time = departure_time >= target_time
+
+        if (arrived_before_or_on_time and left_after_or_on_time):
+            score +=1  
+
+    return score
