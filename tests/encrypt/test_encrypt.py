3c97f9b646587adf9f29ac8ccfa7a5d54366a00f
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-06-08 15:23:12 -0300

    ... novamente

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,3 +3,12 @@
 
 def test_encrypt_message():
     pass
+
+
+def soma(a, b):
+    assert isinstance(a, int) and isinstance(b, int), "Os argumentos devem ser números inteiros"
+    return a + b
+
+resultado = soma(2, 3)
+assert resultado == 5, "A soma está incorreta"
+
