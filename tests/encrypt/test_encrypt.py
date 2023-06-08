24db8112f815b057c81b34243253b008130380d9
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-06-08 15:43:15 -0300

    Ajuste 1.002 num negócio nada a ver que eu nem mexi

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -6,9 +6,11 @@ def test_encrypt_message():
 
 
 def soma(a, b):
-    assert isinstance(a, int) and isinstance(b, int), "Os argumentos devem ser números inteiros"
+    assert isinstance(a, int) and isinstance(
+        b, int
+    ), "Os argumentos devem ser números inteiros"
     return a + b
+
 
 resultado = soma(2, 3)
 assert resultado == 5, "A soma está incorreta"
-
