58b86f918ba728f471d5008836f843a79174eb15
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-06-08 14:41:44 -0300

    corriindo o lint Python

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,13 +4,11 @@ def study_schedule(permanence_period, target_time):
     for period in permanence_period:
         arrival_time = period[0]
         departure_time = period[1]
-        arrival_time_invalid = type(arrival_time) != int
-        departure_time_invalid = type(departure_time) != int
-        target_time_invalid = type(target_time) != int
+        arrival_time_inv = type(arrival_time) != int
+        departure_time_inv = type(departure_time) != int
+        target_time_inv = type(target_time) != int
 
-        if (arrival_time_invalid or
-             departure_time_invalid or 
-             target_time_invalid):
+        if (arrival_time_inv or departure_time_inv or target_time_inv):
             return None
 
         arrived_before_or_on_time = arrival_time <= target_time
