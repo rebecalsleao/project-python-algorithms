97914bdd90fdb465d27ce17ce5ad475bf339cd80
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-06-08 15:40:43 -0300

    Ajuste 1.001 requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -8,13 +8,13 @@ def study_schedule(permanence_period, target_time):
         departure_time_inv = type(departure_time) != int
         target_time_inv = type(target_time) != int
 
-        if (arrival_time_inv or departure_time_inv or target_time_inv):
+        if arrival_time_inv or departure_time_inv or target_time_inv:
             return None
 
         arrived_before_or_on_time = arrival_time <= target_time
         left_after_or_on_time = departure_time >= target_time
 
-        if (arrived_before_or_on_time and left_after_or_on_time):
+        if arrived_before_or_on_time and left_after_or_on_time:
             score += 1
 
     return score
