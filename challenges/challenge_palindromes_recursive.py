89c8f0c310012f617f272af30d5ff349adb8ebbf
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-06-09 15:25:14 -0300

    Requisito 3 - Palidromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    
+    if len(word) == 0:
+        return False
+    
+    if type(low_index) != int and type(high_index) != int:
+        low_index = 0
+        high_index = len(word) - 1
+    
+    if word[low_index] != word[high_index]:
+        return False
+    
+    if low_index + 1 >= high_index - 1:
+        return True
+       
+    return is_palindrome_recursive(word, low_index + 1, high_index -1)
