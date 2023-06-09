51ea4dc095513082de25fac4d64ddd2a5e4224af
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-06-09 12:21:47 -0300

    Ajuste de identação - req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,7 +2,6 @@ import pytest
 from challenges.challenge_encrypt_message import encrypt_message
 
 
-
 def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message(123, 6)
@@ -10,10 +9,10 @@ def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message("abobrinha", "one")
 
-    assert encrypt_message(message = 'beca', key = 4) == 'aceb'
+    assert encrypt_message(message="beca", key=4) == "aceb"
 
-    assert encrypt_message(message ='beca', key = 2) == 'ac_eb'
+    assert encrypt_message(message="beca", key=2) == "ac_eb"
 
-    assert encrypt_message(message = 'beca', key = -1) == 'aceb'
+    assert encrypt_message(message="beca", key=-1) == "aceb"
 
-    assert encrypt_message(message = 'beca', key = 3) == 'ceb_a'
+    assert encrypt_message(message="beca", key=3) == "ceb_a"
